293ae1b31de68c7ac3f4710fccb18a29c465cf34
Ruihan wang <ruihanwang@Ruihans-MacBook-Pro.local>
2022-02-14 00:31:47 -0500

    create friendship follow and unfollow

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -14,6 +14,10 @@ class UserSerializerForTweet(serializers.HyperlinkedModelSerializer):
         fields = ['username', 'id']
 
 
+class UserSerializerForFriendship(UserSerializerForTweet):
+    pass
+
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()

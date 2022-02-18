2522c87771de3136fd781fdb801c4e90588f726d
Ruihan wang <ruihanwang@Ruihans-MacBook-Pro.local>
2022-02-17 20:24:21 -0500

    newfeeds to follows

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,14 +1,21 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 from tweets.models import Tweet
 
 
 class TestCase(DjangoTestCase):
 
+    @property
+    def anonymous_client(self):
+        if hasattr(self, '_anonymous_client'):
+            return self._anonymous_client
+        self._anonymous_client = APIClient()
+        return self._anonymous_client
+
     def create_user(self, username, email=None, password=None):
         if password is None:
             password = 'generic password'
-        # 不能写成 User.objects.create()
         return User.objects.create_user(username, email, password)
 
     def create_tweet(self, user, content=None):
